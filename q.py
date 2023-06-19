313c66b34c1093c72ab425f0154903933ff055d1
zsjsll <124119885@qq.com>
2023-06-19 21:29:02 +0800

    1

--- q.py
+++ q.py
@@ -5,7 +5,19 @@ import uiautomator2 as u2
 
 
 d = u2.connect("4c99a1fa")
+# d = u2.connect("860BDMK229ZQ")
 
-d.screen_on()
-print(d.info)
+if d.info["screenOn"]:
+    pass
+else:
+    d.screen_on()
+    d.swipe(
+        d.info["displayWidth"] * 0.5,
+        d.info["displayHeight"] * 0.9,
+        d.info["displayWidth"] * 0.5,
+        d.info["displayHeight"] * 0.1,
+        duration=0.35,
+    )
+# d.press("volume_mute")
+
 d.app_start("com.alibaba.android.rimet")
